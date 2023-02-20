81a488f741132f1776ecf56b1835a129db8930a4
flavia <flavia.monti95@gmail.com>
2023-02-20 15:28:08 +0100

    path

--- launch_devices.py
+++ launch_devices.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     processes: List[subprocess.Popen] = []
     
-    list_devices = glob.glob(f"actors_api_plan\device_descriptions/[!template]*.json")
+    list_devices = glob.glob(f"actors_api_plan/device_descriptions/*.json")
     for configuration_path in list_devices:
         process = subprocess.Popen([sys.executable, "run-service.py", configuration_path])
         processes.append(process)
