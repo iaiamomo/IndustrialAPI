aee63bdc2e456475bf2e9c0d68b8c16bfa1a2ec3
flavia <flavia.monti95@gmail.com>
2023-03-20 13:44:57 +0100

    phase1_phase22

--- launch_devices.py
+++ launch_devices.py
@@ -7,13 +7,14 @@ import json
 
 config_json = json.load(open('config.json', 'r'))
 mode = config_json['mode']
+phase = config_json['phase']
 
 
 if __name__ == "__main__":
 
     processes: List[subprocess.Popen] = []
     
-    list_devices = glob.glob(f"actors_api_{mode}/descriptions/*.json")
+    list_devices = glob.glob(f"actors_api_{mode}/descriptions_phase{phase}/*.json")
     for configuration_path in list_devices:
         script = f"run_target_{mode}.py" if "target" in configuration_path else f"run_service_{mode}.py"
         process = subprocess.Popen([sys.executable, script, configuration_path])
